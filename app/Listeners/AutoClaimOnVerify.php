0d4a90185e6ee792fc08d750ca587ce0eee13a34
Wessel Verheij <w.verheij@trengo.com>
2022-07-14 15:44:34 +0200

    Add listener to auto-couple claim

---
+++ app/Listeners/AutoClaimOnVerify.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Listeners;
+
+use App\Models\Company;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\InteractsWithQueue;
+
+class AutoClaimOnVerify
+{
+    /**
+     * Create the event listener.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     *
+     * @param  object  $event
+     * @return void
+     */
+    public function handle($event)
+    {
+        $domain = explode('@', $event->user->email_address)[1];
+
+        Company::query()
+            ->where('website', $domain)
+            ->whereNull('user_id')
+            ->update(['user_id' => $event->user->id]);
+    }
+}
